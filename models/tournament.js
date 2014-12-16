f674e691058629899b7a1bf6baad3f64d082303b
Brandon Evans <admin@brandonevans.org>
2014-12-15 23:59:28 -0600

    Connected to a copy of the Tabroom database directly.

---
+++ models/tournament.js
@@ -0,0 +1,45 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+	var Tournament = sequelize.define(
+		'Tournament',
+		{
+			name: {
+				type: DataTypes.STRING(63),
+				allowNull: false
+			},
+			start: DataTypes.DATE,
+			end: DataTypes.DATE,
+			reg_start: DataTypes.DATE,
+			reg_end: DataTypes.DATE,
+			timestamp: {
+				type: DataTypes.DATE,
+				defaultValue: 'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+				allowNull: false
+			},
+			hidden: DataTypes.BOOLEAN,
+			approved: DataTypes.DATE,
+			webname: DataTypes.STRING(63),
+			tz: DataTypes.STRING(31),
+			state: DataTypes.STRING(7),
+			country: DataTypes.STRING(7),
+			foreign_site: DataTypes.INTEGER,
+			foreign_id: DataTypes.INTEGER,
+			created_by: DataTypes.INTEGER,
+		},
+		{
+			timestamps: false,
+			underscored: true,
+			freezeTableName: true,
+			tableName: 'tourn',
+			classMethods: {
+				associate: function(models) {
+					//Tournament.hasMany(models.X)
+					//Tournament.belongsTo(models.Y);
+				}
+			}
+		}
+	);
+
+	return Tournament;
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
-var https = require('https');
-var parseXml = require('libxmljs').parseXml;
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,63 +8,6 @@ router.get('/', function(req, res) {
 });
 
 module.exports = router;
-
-function get_xml(url, res, callback) {
-	https.get(
-		url,
-		function (https_res) {
-			var chunks = [];
-
-			https_res
-			.on(
-				'data',
-				function (chunk) {
-					chunks.push(chunk);
-				}
-			)
-			.on(
-				'end',
-				function () {
-					var xml = Buffer.concat(chunks);
-
-					try {
-						var xmlDoc = parseXml(xml);
-						callback(xmlDoc);
-					}
-					catch (err) {
-						res.status(500);
-						res.json(
-							{
-								'errors': {
-									'status': '500',
-									'title': 'XML Parsing Error',
-									'detail': err.message
-								}
-							}
-						);
-						return;
-					}
-				}
-			);
-		}
-	)
-	.on(
-		'error',
-		function (err) {
-			res.status(500);
-			res.json(
-				{
-					'errors': {
-						'status': '500',
-						'title': 'HTTPS Error',
-						'detail': err.message
-					}
-				}
-			);
-			return;
-		}
-	);
-}
 
 function set_headers(res) {
 	res.set(
@@ -79,33 +21,11 @@ function set_headers(res) {
 router.get(
 	'/tournaments',
 	function (req, res, next) {
-		get_xml(
-			'https://www.tabroom.com/api/current_tournaments.mhtml?timestring=2014-02-14T12%3A00%3A00',
-			res,
-			function (xmlDoc) {
-				var tournaments = [];
-				var tourns = xmlDoc.find('//TOURNLIST/TOURN');
-
-				for (i in tourns) {
-					var tourn = tourns[i];
-
-					tournaments.push(
-						{
-							id: tourn.get('ID').text(),
-							name: tourn.get('TOURNNAME').text(),
-							start_date: tourn.get('STARTDATE').text(),
-							end_date: tourn.get('ENDDATE').text(),
-							download_site: tourn.get('DOWNLOADSITE').text(),
-						}
-					);
-				}
-
+		set_headers(res);
+		models.Tournament.findAll().then(
+			function (tournament) {
 				res.status(200);
-				res.json(
-					{
-						'tournaments': tournaments
-					}
-				);
+				res.json(tournament);
 			}
 		);
 	}
